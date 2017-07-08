5581d5b416257bab5f9fe60852da9282c837e051
Bartek Ćwikłowski <paczesiowa@gmail.com>
2017-07-08 23:26:20 +0200

    Revert "store nb_ptc in column-major order (3x speedup)"
    This reverts commit 02615a869bc39a78f709ec3d300d92cdf05704f5.

--- language_identifier.c
+++ language_identifier.c
@@ -108,10 +108,10 @@ LangID* load_nb_ptc(LangID* model, const char *nb_ptc_path) {
     return NULL;
   }
 
-  /* read all matrix values in column-major order */
+  /* read all matrix values in row-major order */
   for (int i = 0; i < model->nb_ptc_rows; i++) {
     for (int j = 0; j < model->nb_ptc_cols; j++) {
-      if (!read_double(fp, &model->nb_ptc[j * model->nb_ptc_rows + i])) {
+      if (!read_double(fp, &model->nb_ptc[i * model->nb_ptc_cols + j])) {
         printf("Error reading matrix value\n");
         fclose(fp);
         return NULL;
@@ -358,7 +358,7 @@ void check_model(LangID *model) {
   printf("%zu %zu\n", model->nb_ptc_rows, model->nb_ptc_cols);
   for (int i = 0; i < model->nb_ptc_rows; i++) {
     for (int j = 0; j < model->nb_ptc_cols; j++) {
-      printf("nb_ptc[%d, %d] = %.2f\n", i, j, model->nb_ptc[j * model->nb_ptc_rows + i]);
+      print_double(model->nb_ptc[i * model->nb_ptc_cols + j]);
     }
   }
   printf("%zu\n", model->nb_pc_length);
@@ -426,7 +426,7 @@ double classify(LangID *model, const char *text, char *language) {
   int cl = 0;
   for (int i = 0; i < model->nb_ptc_cols; i++) {
     for (int j = 0; j < model->nb_ptc_rows; j++) {
-      pdc[i] += arr[j] * model->nb_ptc[i * model->nb_ptc_rows + j];
+      pdc[i] += arr[j] * model->nb_ptc[j * model->nb_ptc_cols + i];
     }
     if (pdc[i] > pdc[cl]) {
       cl = i;
@@ -454,7 +454,7 @@ void benchmark(LangID *model) {
   char *text = "quick brown fox jumped over the lazy dog";
   struct timeval t0, t1;
   gettimeofday(&t0, 0);
-  int run_count = 50;
+  int run_count = 5000;
   for (int i = 0; i < run_count; i++) {
     classify(model, text, language);
   }
